869519b9f2ed3f89d29479ac608538707fb23470
EllenSamanta <ellen.samanta@outlook.com>
2026-01-23 14:49:27 -0300

    chore: criando função para adicionar colaborador na ata

--- Api-segura/api/src/main/java/com/api/seguranca/api/controller/WorkshopController.java
+++ Api-segura/api/src/main/java/com/api/seguranca/api/controller/WorkshopController.java
@@ -4,6 +4,11 @@ import com.api.seguranca.api.repository.WorkshopRepository;
 import com.api.seguranca.api.model.WorkshopEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import com.api.seguranca.api.repository.ColaboradorRepository;
+import com.api.seguranca.api.model.AtaEntity;
+import com.api.seguranca.api.model.ColaboradorEntity;
+import com.api.seguranca.api.repository.AtaRepository;
+
 
 @RestController
 @RequestMapping("/api/workshops")
@@ -12,8 +17,39 @@ public class WorkshopController {
     @Autowired
     private WorkshopRepository workshopRepository;
 
+    @Autowired
+    private AtaRepository ataRepository;
+
+    @Autowired
+    private ColaboradorRepository colaboradorRepository;
+
     @PostMapping
     public WorkshopEntity criarWorkshop(@RequestBody WorkshopEntity workshop) {
         return workshopRepository.save(workshop);
     }
+
+    @PutMapping("/{workshopId}/atas/{ataId}/colaboradores/{colaboradorId}")
+    public AtaEntity adicionarColaboradorNaAta(
+            @PathVariable Integer workshopId,
+            @PathVariable Integer ataId,
+            @PathVariable Integer colaboradorId
+    ) {
+        AtaEntity ata = ataRepository
+                .findByIdAndWorkshopId(ataId, workshopId)
+                .orElseThrow(() -> new RuntimeException("Ata não encontrada para este workshop"));
+
+        ColaboradorEntity colaborador = colaboradorRepository
+                .findById(colaboradorId)
+                .orElseThrow(() -> new RuntimeException("Colaborador não encontrado"));
+
+        if (!ata.getColaboradores().contains(colaborador)) {
+            ata.getColaboradores().add(colaborador);
+        }
+
+        return ataRepository.save(ata);
+    }
+
+
+
+
 }
