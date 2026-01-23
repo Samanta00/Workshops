1404db72ac1c3b0714e9d7376e4fc4ce4d955250
EllenSamanta <ellen.samanta@outlook.com>
2026-01-23 14:48:34 -0300

    chore: listando workshops

--- Api-segura/api/src/main/java/com/api/seguranca/api/repository/WorkshopRepository.java
+++ Api-segura/api/src/main/java/com/api/seguranca/api/repository/WorkshopRepository.java
@@ -1,11 +1,12 @@
 package com.api.seguranca.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 import com.api.seguranca.api.model.WorkshopEntity;
 
 
 public interface WorkshopRepository extends JpaRepository <WorkshopEntity, Integer>{
-    
+        List<WorkshopEntity> findByNomeContainingIgnoreCase(String nome);
 }
 
