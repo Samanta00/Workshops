0f6d7ef43e0f06e799522b5f87ab1291cd45bb6a
EllenSamanta <ellen.samanta@outlook.com>
2026-01-23 08:25:01 -0300

    lint: remoção de arquivos desnecessários no projeto

--- Api-segura/api/src/main/java/com/api/seguranca/api/Repository/RepositoryRegister.java
+++
@@ -1,9 +0,0 @@
-package com.api.seguranca.api.Repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.api.seguranca.api.Model.Register;
-
-public interface RepositoryRegister extends CrudRepository <Register, Integer>{
-    
-}
