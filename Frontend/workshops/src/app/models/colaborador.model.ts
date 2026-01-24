082c35f50c8dac7fc33e00339391f9727e2078c8
EllenSamanta <ellen.samanta@outlook.com>
2026-01-24 10:22:33 -0300

    chore: criando de colaborador model

---
+++ Frontend/workshops/src/app/models/colaborador.model.ts
@@ -0,0 +1,26 @@
+export interface Colaborador {
+    id: number;
+    nome: string;
+}
+  
+//   // workshop.model.ts
+//   import { Colaborador } from './colaborador.model';
+  
+//   export interface Workshop {
+//     id: number;
+//     nome: string;
+//     dataRealizacao: Date;
+//     descricao: string;
+//     colaboradores: Colaborador[];
+//   }
+  
+//   // ata.model.ts
+//   import { Workshop } from './workshop.model';
+//   import { Colaborador } from './colaborador.model';
+  
+//   export interface Ata {
+//     id: number;
+//     workshop: Workshop;
+//     colaboradores: Colaborador[];
+//   }
+  
